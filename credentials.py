b7b19a5c8fda86e23bbcc4468b467eceaf26d7ca
jidraph kimachia <jidraphkimachia1@gmail.com>
2022-04-23 14:55:42 +0300

    user and credentials class

---
+++ credentials.py
@@ -0,0 +1,4 @@
+class Credentials:
+    """
+    A class that generates new instances of our password 
+    """
---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    """
+    Class that generates new instances of the User
+    """
+
+    user_list = [] #empty user-list
+
+    def __init__(self, username, password):
+        """
+    Instance that creates the defined properties for our object
+    """
