f617d5aa041650644bec3704e65dbd3d598fe31c
jidraph kimachia <jidraphkimachia1@gmail.com>
2022-04-23 03:07:40 +0300

    Random Choice

---
+++ PassGenerator/generator.py
@@ -0,0 +1,14 @@
+import random
+#random values generator form a given range
+
+characters = "abcdefghijklmnopqrstuvwxABCDEFGHIJKLMNOPQRSTUVWXYZ" #The character range for the password
+
+while 1: #While 1 is an infinite loop, meaning it will run all through the code
+    pass_len = int(input("How long would you like your new password to be? We suggest a long password for security purposes: "))
+    pass_count = int(input("How many passwords would you like generated? "))
+
+    for i in range(0, pass_count):
+        password = ""
+        for i in range(0, pass_len):
+            password_characters = random.choice(characters)
+            
