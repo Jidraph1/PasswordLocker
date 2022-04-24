4f5baccfd2215231ac66b07db0347f3dfb4dd057
jidraph kimachia <jidraphkimachia1@gmail.com>
2022-04-24 09:47:41 +0300

    main functions in run

--- run.py
+++ run.py
@@ -22,4 +22,34 @@ def delete_user(user):
     """
     user.delete_user()
 
-de
+def create_credentials(account_name, account_password):
+    """
+    Function to create new credentials
+    """
+    new_cred = Credentials(account_name, account_password)
+    return new_cred
+
+def save_credentials(credentials):
+    """
+    function that saves new credentials
+    """
+    credentials.save_credentials()
+
+def delete_credentials(credentials):
+    """
+    function that deletes credentials
+    """
+    credentials.delete_credentials()
+
+def get_credentials(account_name):
+    """
+    Function that finds a credential and returns it
+    """
+    return Credentials.get_credentials(account_name)
+
+def display_credentials():
+    """
+    function that returns saved credentials
+    """
+
+    return Credentials.display_credentials()
